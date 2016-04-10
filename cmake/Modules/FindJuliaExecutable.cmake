63b02560965224a637d7b9d7bef59186202d155e
ALEXANDER PLETZER <axp5530@psu.edu>
2016-04-10 15:24:07 +1200

    re-organizing

---
+++ cmake/Modules/FindJuliaExecutable.cmake
@@ -0,0 +1,4 @@
+find_program(JULIA_EXECUTABLE
+  NAMES julia)
+mark_as_advanced(JULIA_EXECUTABLE)
+
---
+++ cmake/Modules/FindPythonExecutable.cmake
@@ -0,0 +1,4 @@
+find_program(PYTHON_EXECUTABLE
+  NAMES python)
+mark_as_advanced(PYTHON_EXECUTABLE)
+
