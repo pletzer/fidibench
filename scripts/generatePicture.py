0e68557d05138458ec96c68fb3c06a26e8d45cfb
pletzera <pletzera@niwa-1007638.niwa.local>
2016-05-07 12:26:17 +1200

    now able to plot

--- scripts/generatePicture.py
+++ scripts/generatePicture.py
@@ -11,27 +11,54 @@ args = parser.parse_args()
 
 
 reader = vtk.vtkRectilinearGridReader()
+cell2pt = vtk.vtkCellDataToPointData()
 contour = vtk.vtkContourFilter()
 mapper = vtk.vtkPolyDataMapper()
 actor = vtk.vtkActor()
 
+reader.SetFileName(args.filename)
+reader.ReadAllScalarsOn()
+
+numCont = 11
+fmin, fmax = 0., 1.
+contour.SetNumberOfContours(numCont)
+for i in range(numCont):
+    contour.SetValue(i, fmin + (fmax - fmin)*i/float(numCont - 1))
+contour.GenerateTrianglesOn()
+contour.ComputeGradientsOn()
+
+mapper.ScalarVisibilityOff()
+
+actor.GetProperty().SetColor(1.0, 0., 0.)
+
 # Connect
-contour.SetInputConnection(reader.GetOutputPort())
+pdata = reader.GetOutput()
+cell2pt.SetInputConnection(reader.GetOutputPort())
+contour.SetInputConnection(cell2pt.GetOutputPort())
 mapper.SetInputConnection(contour.GetOutputPort())
 actor.SetMapper(mapper)
 
-reader.SetFileName(args.filename)
-reader.Update()
+mapper.Update()
 
-contour.SetNumberOfContours(1)
-contour.SetValue(0, 0.5)
+#mapper.SetScalar('f')
+#print 'min/max f range: ', mapper.GetRange()
+#pdata = contour.GetOutput()
+#print pdata
+
+numScalars = reader.GetNumberOfScalarsInFile()
+for i in range(numScalars):
+    print 'scalar field: ', reader.GetScalarsNameInFile(i)
+
+print contour.GetOutput()
 
 # Create the renderer, the render window, and the interactor. The
 # renderer draws into the render window, the interactor enables mouse-
 # and keyboard-based interaction with the scene.
 ren = vtk.vtkRenderer()
 ren.AddActor(actor)
+ren.SetBackground(0.5, 0.5, 0.5)
 renWin = vtk.vtkRenderWindow()
+renWin.SetSize(600, 500)
 renWin.AddRenderer(ren)
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
