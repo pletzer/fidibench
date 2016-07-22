c1ccc22806f48702d4dd5a08d6c96e67e3bdf8fa
pletzera <pletzera@niwa-1007520.niwa.local>
2016-07-23 09:17:59 +1200

    + gpu desktop results

--- scripts/fdi_upwind_openmp.py
+++ scripts/fdi_upwind_openmp.py
@@ -22,6 +22,29 @@ results {
 	'idefix': {},
 	'pan-wm': {},
 	'pan-sb': {},
-	''
+	'niwa-1007520': {
+                'cpu': 'Intel Xeon 3.7GHz',
+                'compiler': 'gnu-4.8.5',
+                'num cores': 4, # to check
+                'time_s': {
+                        1: 18.7,
+                        2: 12.7,
+                        4: 8.26,
+                        8: 5.46,
+                        16: 4.90,
+                        32: 4.56,
+                        64: 4.35,
+        },
+        'niwa-1007838': {
+                'cpu': 'Intel i5 2.3GHz',
+                'compiler': 'gnu-4.8.5',
+                'num cores': 2, 
+                'time_s': {
+                        1: ,
+                        2: ,
+                        4: ,
+                        8: ,
+        }
+
 	
 }
