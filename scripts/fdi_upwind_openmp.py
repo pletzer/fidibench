fc743df09f4dd180f6b58526a10d84474234a9dc
Alex Pletzer <alexander@gokliya.net>
2016-07-24 07:19:49 +1200

    + plotting

--- scripts/fdi_upwind_openmp.py
+++ scripts/fdi_upwind_openmp.py
@@ -9,7 +9,7 @@ from matplotlib import pylab
 results = {
 	'abraracourcix': {
 		'cpu': 'Intel i7',
-                'clock GHz': 2.2,
+        'clock GHz': 2.2,
 		'compiler': 'gnu-6.1',
 		'num cores': 4,
 		'time s': {	
@@ -18,43 +18,36 @@ results = {
 			4: 9.5,
 			8: 7.7,
 			16: 7.7,
-                        },
 		},
 	},
-	'idefix': {},
-	'pan-wm': {},
-	'pan-sb': {},
 	'niwa-1007520': {
-                'cpu': 'Intel Xeon',
-                'clock GHz': 3.7,
-                'compiler': 'gnu-4.8.5',
-                'num cores': 4, # to check
-                'time s': {
-                        1: 18.7,
-                        2: 12.7,
-                        4: 8.26,
-                        8: 5.46,
-                        16: 4.90,
-                        32: 4.56,
-                        64: 4.35,
-                        },
-                },
+        'cpu': 'Intel Xeon',
+        'clock GHz': 3.7,
+        'compiler': 'gnu-4.8.5',
+        'num cores': 4, # to check
+        'time s': {
+            1: 18.7,
+            2: 12.7,
+            4: 8.26,
+            8: 5.46,
+            16: 4.90,
+            32: 4.56,
+            64: 4.35,
         },
-        'niwa-1007838': {
-                'cpu': 'Intel i5',
-                'clock GHz': 2.3,
-                'compiler': 'gnu-4.8.5',
-                'num cores': 2, 
-                'time s': {
-                        1: float('nan'),
-                        2: float('nan'),
-                        4: float('nan'),
-                        8: float('nan'),
-                        },
-                },
-        }
+    },
+    'niwa-1007838': {
+        'cpu': 'Intel i5',
+        'clock GHz': 2.3,
+        'compiler': 'gnu-4.8.5',
+        'num cores': 2, 
+        'time s': {
+            1: float('nan'),
+            2: float('nan'),
+            4: float('nan'),
+            8: float('nan'),
+        },
+    },
 
-	
 }
 
 # plot speed vs clock (1 thread)
@@ -62,9 +55,17 @@ clock = []
 speed = []
 cpu = []
 for name, data in results.items():
-        cpu.append(data['cpu'])
-        clock.append(data['clock GHz'])
-        speed.append(1./float(data['time s'][1]))
+    cpu.append(data['cpu'])
+    clock.append(data['clock GHz'])
+    speed.append(1./float(data['time s'][1]))
 
 import matplotlib.pyplot as plt
-plt.scatter(clock, speed)
+fig, ax = plt.subplots()
+ax.scatter(clock, speed)
+for i in range(len(clock)):
+    ax.annotate(cpu[i], (clock[i], speed[i]))
+plt.xlabel('Clock GHz')
+plt.ylabel('Speed 1/s')
+plt.title('Speed vs clock frequency')
+#plt.axes([0, 5, 0, 0.1])
+plt.show()
