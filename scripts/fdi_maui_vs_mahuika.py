e5e22e10ece30b16c18159939d7f21f12c610fa6
Alex Pletzer <alexander@gokliya.net>
2019-08-23 14:57:33 +1200

    latest numbers

--- scripts/fdi_maui_vs_mahuika.py
+++ scripts/fdi_maui_vs_mahuika.py
@@ -2,6 +2,15 @@ from matplotlib import rcParams, pylab
 
 rcParams['font.size'] = 14
 
+"""
+export OMP_NUM_THREADS=2; export OMP_PROC_BIND=true; export OMP_PLACES=cores; srun --nodes=1 --ntasks=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --mem=5g map --profile ./upwindCxx -numSteps 10 -numCells 512
+
+mahuika
+for n in 36 18 9 4 2 1; do echo $n; export OMP_NUM_THREADS=$n; export OMP_PROC_BIND=true; export OMP_PLACES=cores; srun --nodes=1 --ntasks=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --mem=5g time ./upwindCxx -numSteps 10 -numCells 512; done
+
+maui
+for n in 40 20 10 4 2 1; do echo $n; export OMP_NUM_THREADS=$n; export OMP_PROC_BIND=true; export OMP_PLACES=cores; srun --nodes=1 --ntasks=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --mem=5g time ./upwindCxx -numSteps 10 -numCells 512; done
+"""
 
 def plotOld(keys):
 	for k in keys:
@@ -24,28 +33,28 @@ def plotNew(keys):
 	pylab.xlabel('num threads')
 	pylab.ylabel('time [s]')
 	pylab.title('fidibench upwind C++ 10 steps/512^3 cells')
-	pylab.axis([1, 40, 0, 800])
+	pylab.axis([1, 40, 0, 150])
 	pylab.show()
 
 data = {
 	'mahuika-cray': {
 		'nthreads': [36, 18, 9, 4, 2, 1],
 		'seconds-old' : [131, 102, 102, 86, 130, 189],
-		'seconds-new' : [6.2, 11, 19, 36, 72, 142],
+		'seconds-new' : [6.7, 10.1, 21.3, 36.3, 74.4, 145],
 		'color': 'r',
 		'style': '--'
 		},
 	'mahuika-intel': {
 		'nthreads': [36, 18, 9, 4, 2, 1],
 		'seconds-old' : [31, 68, 78, 109, 303, 182],
-		'seconds-new' : [6.2, 10, 17, 36, 75, 138],
+		'seconds-new' : [6.2, 9.8, 17.5, 35.8, 70, 139],
 		'color': 'g',
 		'style': '--'
 		},
 	'mahuika-gnu': {
 		'nthreads': [36, 18, 9, 4, 2, 1],
 		'seconds-old' : [ 22, 63, 84, 132, 197, 177,], # [45, 180, 236, 361, 447, 807],
-		'seconds-new' : [ 6.1, 9.5, 17.3, 35.2, 68.6, 134], # [18, 32, 58, 124, 242, 483],
+		'seconds-new' : [ 6.5, 10.2, 20.6, 37.5, 73.0, 134], # [18, 32, 58, 124, 242, 483],
 		'color': 'b',
 		'style': '--'
 		},
@@ -53,7 +62,7 @@ data = {
 	'maui-cray': {
 		'nthreads': [40, 20, 10, 4, 2, 1],
 		'seconds-old' : [7.3, 12, 18, 39, 71, 124],
-		'seconds-new' : [6.5, 9.8, 14, 27, 50, 96],
+		'seconds-new' : [6.5, 9.9, 13.9, 27.8, 51.0, 98.4],
 		'color': 'r',
 		'style': '-'
 		},
@@ -61,7 +70,7 @@ data = {
 	'maui-intel': {
 		'nthreads': [40, 20, 10, 4, 2, 1],
 		'seconds-old' : [27, 33, 81, 99, 183, 126],
-		'seconds-new' : [5.9, 8.6, 13, 28, 50, 96],
+		'seconds-new' : [5.8, 8.5, 13.0, 27.5, 49.5, 94.7],
 		'color': 'g',
 		'style': '-'
 		},
@@ -69,14 +78,15 @@ data = {
 	'maui-gnu': {
 		'nthreads': [40, 20, 10, 4, 2, 1],
 		'seconds-old' : [32, 32, 26, 85, 137, 115],
-		'seconds-new' : [6.4, 9.0, 13.2, 26.8, 48, 90],
+		'seconds-new' : [6.6, 9.1, 13.4, 27.1, 47.8, 90.8],
 		'color': 'b',
 		'style': '-'
 		},
 
 }
 
-plotOld(keys=['mahuika-' + p for p in ('cray', 'intel', 'gnu')])
-plotOld(keys=['maui-' + p for p in ('cray', 'intel', 'gnu')])
+plotNew(keys=['mahuika-' + p for p in ('cray', 'intel', 'gnu')])
+plotNew(keys=['maui-' + p for p in ('cray', 'intel', 'gnu')])
+plotNew(keys=['mahuika-' + p for p in ('cray', 'intel', 'gnu')] + ['maui-' + p for p in ('cray', 'intel', 'gnu')])
 
 
